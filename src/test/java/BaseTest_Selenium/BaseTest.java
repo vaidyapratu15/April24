262e2430453f945e65122db17b345f1f38548077
pratikshaQA <pratiksha.vaidya@wohlig.com>
2024-04-19 10:39:11 +0530

    ListBox

---
+++ src/test/java/BaseTest_Selenium/BaseTest.java
@@ -0,0 +1,87 @@
+package BaseTest_Selenium;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.time.Duration;
+import java.awt.Desktop;
+import org.apache.commons.mail.EmailException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.asserts.SoftAssert;
+
+import com.aventstack.extentreports.Status;
+import com.global.library.BaseClass;
+import com.global.library.Utilities;
+import com.global.pomclass.LoginPage;
+
+public class BaseTest extends BaseClass{
+
+    @BeforeSuite
+	public void lunchBrowser() {  // here not used webdriver manager and not extends baseclass setup browser
+		driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@AfterSuite
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(8000);
+		driver.quit();
+	}
+	
+	@BeforeTest
+	public void getExtentReport() {
+		setExtentReport();
+	}
+	
+	@AfterTest
+	public void viewReport() throws IOException {
+		extent.flush();
+		Desktop.getDesktop().browse(new File("Extent Report.html").toURI());
+	}
+	
+	@BeforeMethod
+	public void createTest(ITestResult result , Method m) {
+		String testName = result.getMethod().getRealClass().getSimpleName()+ " - " + result.getMethod().getMethodName();
+		test = extent.createTest(testName , result.getMethod().getDescription()); 
+	}
+	
+	@AfterMethod
+	public void getStatus(ITestResult result , Method m) throws IOException {
+		if ( result.getStatus() == ITestResult.FAILURE) {
+	     Utilities.CaptureScreenshots(result.getMethod().getMethodName(), driver);
+	     test.log(Status.FAIL, m.getName());
+		}else {
+			test.log(Status.PASS, m.getName());
+		}
+	}
+	
+	
+	@AfterSuite
+	public void setEmail() throws EmailException {
+//		MailTestClass.setAttachEmailReport();
+	}
+	
+	@BeforeClass
+	public void pomPageObject() {
+	 soft = new SoftAssert(); 
+	 loginPage = new LoginPage(driver);
+	}
+	
+	protected SoftAssert soft;
+	protected LoginPage loginPage;
+	
+
+
+
+
+    
+}
---
+++ src/test/java/ListBox/ListBox.java
@@ -0,0 +1,116 @@
+package ListBox;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import BaseTest_Selenium.BaseTest;
+
+public class ListBox extends BaseTest{
+   
+
+    @Test (priority = 0)
+    public void openBrowse(){  // this program weote in file.html
+        driver.get("http://127.0.0.1:5500/src/test/java/ListBox/file.html");
+        test.info(driver.getCurrentUrl());
+
+    }
+
+    @Test (priority = 1)
+    public void selectOptionFromMultiselectBox() throws InterruptedException{
+
+       WebElement selectCountry= driver.findElement(By.id("1234"));
+
+       Select s = new Select(selectCountry);
+
+        s.selectByVisibleText("IND");
+        s.selectByIndex(2);   
+        s.selectByVisibleText("Sri");
+       // s.selectByValue("11"); here no value present 
+       Thread.sleep(4000);
+
+       test.info("----Check is multiple or not ----");
+
+
+       boolean result = s.isMultiple();
+       if (result ==true){
+        test.info("is multiSlectable");
+       }else{
+        test.info("is singly Slectable"); 
+       }
+       Thread.sleep(4000);
+
+
+       test.info("---- Get first selected option ----");
+       String text = s.getFirstSelectedOption().getText();
+       test.info(text);
+     
+      Thread.sleep(4000);
+
+      test.info("---- Get all selected option ----");
+       List<WebElement> alloption = s.getAllSelectedOptions();
+       
+       for (WebElement xx : alloption){
+        test.info(xx.getText());
+       }
+     
+      Thread.sleep(4000);
+
+    }
+   
+
+    @Test (priority = 2)
+    public void deSelectOptionFromMultiselectBox() throws InterruptedException{
+    
+       WebElement selectCountry= driver.findElement(By.id("1234"));
+
+       Select s = new Select(selectCountry);
+
+        s.selectByIndex(1);   
+        s.selectByIndex(2);  
+        s.selectByVisibleText("Sri");    Thread.sleep(4000);
+       // s.selectByValue("11"); here no value present   
+      
+
+       s.deselectByIndex(1);   Thread.sleep(4000);
+
+       s.deselectAll();
+
+       test.info("---- Get all option present ----");
+       List<WebElement> alloption = s.getOptions();
+       
+       for (WebElement xx : alloption){
+       test.info(xx.getText());
+       }
+
+
+
+    }
+
+// for facebook
+public  void useSelectMethod() throws InterruptedException{
+    driver.get("https://www.facebook.com/");
+
+    WebElement  ele= driver.findElement(By.xpath("//div//a[text()='Create new account']"));
+    JavascriptExecutor js = (JavascriptExecutor)driver;
+    js.executeScript("arguments[0].click();", ele);
+      
+     WebElement month= driver.findElement(By.id("month"));
+
+     Select s = new Select(month);
+
+    // s.selectByIndex(10);
+     //s.selectByValue("11");
+     s.selectByVisibleText("Nov");
+     Thread.sleep(4000);
+}
+
+
+
+
+
+
+    
+}
